fad22153ae12278e211f38721902b4729a9fb8ee
Ondrej Vana <ondrej.vana@keboola.com>
2013-04-30 14:32:55 -0700

    Added access token, redirect to cover photo if available as a last option before fallback.

--- fbimgparser.php
+++ fbimgparser.php
@@ -1,11 +1,12 @@
 <?php
 $FBGraphUrl = "https://graph.facebook.com/";					// Facebook API URL
 $size = is_numeric(@$_GET['size']) ? @$_GET['size'] : 320;		// Default image size
+$token = "BAAAAUQC61RkBAGuHc74A5QHoGsHCOZBzTuAAOjgE3bzEcRkzPdIoY1eh0UKXZAiOFZBpEubx2aN3ARTYypKOCuPowZCw6wNdghMGl0rMvOE1DquU9IcSsBzvUBf4SixFJqwV2ZASQMMursOZBzdIYTw6stZCfLlkXEuw1K5mQrgSbZBSAulVDmndGZBq3bvGAzZCUZD";
 
 if(isset($_GET['id'])) {
-	$json = @file_get_contents($FBGraphUrl . $_GET['id'], 0, null, null);
+    $FBGraphUrl = $FBGraphUrl . $_GET['id'] . "?access_token=" . $token;
+	$json = @file_get_contents($FBGraphUrl, 0, null, null);
 	$FBData = json_decode($json);
-	
 	if(isset($FBData->images)) {
 		foreach($FBData->images as $img) {
 			if($img->width == $size){
@@ -15,7 +16,7 @@ if(isset($_GET['id'])) {
 				$images[] = $img;
 			}
 		}
-		
+
 		if(!isset($resultUrl) && isset($images)) {
 			$curSize = 0;
 			foreach($images as $img) {
@@ -26,11 +27,15 @@ if(isset($_GET['id'])) {
 			}
 		}
 	}
-	
+
 	if(!isset($resultUrl) && isset($FBData->picture)) {
 		$resultUrl = $FBData->picture;
 	}
-	
+
+    if(!isset($resultUrl) && isset($FBData->cover_photo)) {
+        header("Location: " . $_SERVER['PHP_SELF'] . "?id=" . $FBData->cover_photo . "&size=" . $size); // redirect to self!
+    }
+
 	if(!isset($resultUrl)) {
 		$resultUrl = "https://keboola-bi.s3.amazonaws.com/liveworld/LW_No_image.jpg";
 	}
